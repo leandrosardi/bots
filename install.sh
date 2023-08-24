2a8e80249f7f3d196b16538e25b5b21b236df0a2
leandrosardi <leandro.sardi@expandedventure.com>
2023-08-24 13:19:30 -0300

    installation

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget http://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
+sudo dpkg -i google-chrome-stable_114.0.5735.90-1_amd64.deb
+sudo apt-get install -f
+google-chrome --version
