5c0184d512e54f639e50d3589d602aa21bd3492b
leandrosardi <leandro.sardi@expandedventure.com>
2023-08-24 13:29:32 -0300

    installation

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# Reference:
+# - https://skolo.online/documents/webscrapping/#step-1-download-chrome
+# - https://stackoverflow.com/questions/50642308/webdriverexception-unk
+#
 sudo apt update
 wget http://mirror.cs.uchicago.edu/google-chrome/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
 sudo dpkg -i google-chrome-stable_114.0.5735.90-1_amd64.deb
