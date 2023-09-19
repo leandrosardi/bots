e29d9842ead02bb4d75730a611f9480f4a6b2b82
leandrosardi <leandro.sardi@expandedventure.com>
2023-09-19 17:57:56 -0300

    adding proxy suport

--- examples/indeed.rb
+++ examples/indeed.rb
@@ -1,28 +1,37 @@
-require 'bots'
+require_relative '../lib/bots'
 
-l = BlackStack::LocalLogger.new('./indeed1.log')
+tag = 'Jacksonville'
+
+l = BlackStack::LocalLogger.new("./indeed.#{tag}.log")
 
 puts "\n\nEXAMPLE 1".blue
 puts "Find domain of a company using Google Search API.\nCount the number of 'not-found' and 'error' cases.\n".yellow
 
 l.logs 'initialize GoogleBot... '
-bot = BlackStack::Bots::Indeed.new(proxy: nil)
+bot = BlackStack::Bots::Indeed.new(
+    ip: '192.151.150.90',
+    user: nil,
+    password: nil,
+    port_from: 19010,
+    port_to: 19010
+)
 l.done
 
 output_filename = 'indeed2'
 start = 0
-search = "https://www.indeed.com/q-full-time-$85,000-l-Jacksonville,-FL-jobs.html?vjk=c344d10b0d9d6b38"
+search = "https://www.indeed.com/jobs?q=%2435%2C000&l=Jacksonville%2C+FL&radius=25&vjk=4d50a7da37ac13e8"
 while start <= 640
-    l.logs "start=#{start}... "
+begin
+    l.logs "start=#{start.to_s.blue}... "
 
     url = "#{search}&start=#{start}"
     ret = bot.results(url)
 
     # save ret into a json file
-    File.open("./#{output_filename}.start-#{start.to_s}.json", 'w') { |f| f.write(ret.to_json) }
+    #File.open("./#{output_filename}.start-#{start.to_s}.json", 'w') { |f| f.write(ret.to_json) }
 
     # save into a CSV file
-    CSV.open("./#{output_filename}.all.csv", 'a+b') { |csv|
+    CSV.open("./#{output_filename}.#{tag}.csv", 'a+b') { |csv|
         csv << ['title', 'url', 'company', 'location', 'salary', 'posted', 'snippets']
         ret.each { |h|
             csv << [h[:title], h[:url], h[:company], h[:location], h[:salary], h[:posted], h[:snippets].join(' / ')]
@@ -32,5 +41,12 @@ while start <= 640
     # increase start
     start += 10
 
-    l.done
+    l.logf 'done'.green
+# catch when user press CTRL+C
+rescue Interrupt => e
+    l.logf "Process canceled by user".yellow
+    exit(0)
+rescue Exception => e
+    l.logf "ERROR: #{e.message}".red
+end
 end
--- lib/indeed.rb
+++ lib/indeed.rb
@@ -5,45 +5,53 @@ module BlackStack
 
             def results(url, page=1)
                 ret = []
-                # launch a chrome browser with selenium
-                driver = Selenium::WebDriver.for :chrome
-                browser = driver.browser
-                # TODO: set a proxy with user and password
-                driver.get url
-                # get the ul list with class .jobsearch-ResultsList
-                ul = driver.find_element(:class=>'jobsearch-ResultsList')
-                # scroll to the bottom
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-                # iterate li elements
-                i = 0
-                ul.find_elements('css', 'li').each { |li|
-                    h = {}
-                    i += 1
-                    links = li.find_elements('css', 'a.jcs-JobTitle')
-                    if links.size == 1
-                        link = li.find_element('css', 'a.jcs-JobTitle')
-                        h[:title] = link.text
-                        h[:url] = link.attribute('href')
-                    
-                        o = li.find_elements('css','span.companyName').first
-                        h[:company] = o ? o.text : ''
+                # launch a chrome browser with selenium, with proxy
+                # refernece: https://www.browserstack.com/guide/set-proxy-in-selenium
+                opts = Selenium::WebDriver::Chrome::Options.new
+                opts.add_argument('--proxy-server=173.208.150.242:15001')
+                # start a chrome browser using the proxy configured above
+                driver = Selenium::WebDriver.for(:chrome, :options=>opts)
+                begin
+                    browser = driver.browser
 
-                        o = li.find_elements('css','div.companyLocation').first
-                        h[:location] = o ? o.text : ''
+                    # TODO: set a proxy with user and password
+                    driver.get url
+                    # get the ul list with class .jobsearch-ResultsList
+                    ul = driver.find_element(:id=>'mosaic-provider-jobcards').find_element(:css=>'ul')
+                    # scroll to the bottom
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+                    # iterate li elements
+                    i = 0
+                    ul.find_elements('css', 'li').each { |li|
+                        h = {}
+                        i += 1
+                        links = li.find_elements('css', 'a.jcs-JobTitle')
+                        if links.size == 1
+                            link = li.find_element('css', 'a.jcs-JobTitle')
+                            h[:title] = link.text
+                            h[:url] = link.attribute('href')
                         
-                        o = li.find_elements('css','div.salary-snippet-container').first
-                        h[:salary] = o ? o.text : ''
-                        
-                        o = li.find_elements('css','span.date').first
-                        h[:posted] = o ? o.text.gsub("Posted\nPosted", '').strip : ''
-                        
-                        h[:snippets] = li.find_elements('css','div.job-snippet > ul > li').map { |li| li.text }
-                        
-                        ret << h
-                    end
-                }
-                # destroy selenium browser
-                driver.quit
+                            o = li.find_elements('css','span.companyName').first
+                            h[:company] = o ? o.text : ''
+
+                            o = li.find_elements('css','div.companyLocation').first
+                            h[:location] = o ? o.text : ''
+                            
+                            o = li.find_elements('css','div.salary-snippet-container').first
+                            h[:salary] = o ? o.text : ''
+                            
+                            o = li.find_elements('css','span.date').first
+                            h[:posted] = o ? o.text.gsub("Posted\nPosted", '').strip : ''
+                            
+                            h[:snippets] = li.find_elements('css','div.job-snippet > ul > li').map { |li| li.text }
+                            
+                            ret << h
+                        end
+                    }
+                ensure
+                    # destroy selenium browser
+                    driver.quit
+                end
                 # return
                 ret
             end # results
