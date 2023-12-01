80a856078f104ab8b0c8f393290086d73d0c92a3
leandrosardi <leandro.sardi@expandedventure.com>
2023-12-01 16:13:21 -0300

    playing with adspower

--- examples/adspower.rb
+++ examples/adspower.rb
@@ -3,6 +3,8 @@ require 'net/http'
 require 'json'
 require 'pry'
 require 'blackstack-core'
+require 'selenium-webdriver'
+require 'watir'
 
 # reference: https://localapi-doc-en.adspower.com/
 # reference: https://localapi-doc-en.adspower.com/docs/Rdw7Iu
@@ -39,7 +41,7 @@ def create
     })
     # show respose body
     ret = JSON.parse(res.body)
-    raise "Error: #{ret.to_s}" if ret['msg'] != 'Success'
+    raise "Error: #{ret.to_s}" if ret['msg'].to_s.downcase != 'success'
     # return id of the created user
     ret['data']['id']
 end
@@ -55,23 +57,35 @@ def delete(id)
     # show respose body
     ret = JSON.parse(res.body)
     # validation
-    raise "Error: #{ret.to_s}" if ret['msg'] != 'Success'
+    raise "Error: #{ret.to_s}" if ret['msg'].to_s.downcase != 'success'
 end
 
-# open the browser
+# run the browser
 # return the URL to operate the browser thru selenium
 # 
 # reference: https://localapi-doc-en.adspower.com/docs/FFMFMf
 # 
-def open(id)
+def start(id)
     uri = URI.parse("#{@url}/api/v1/browser/start?user_id=#{id}")
     res = Net::HTTP.get(uri)
     # show respose body
     ret = JSON.parse(res)
-    raise "Error: #{ret.to_s}" if ret['msg'] != 'Success'
+    raise "Error: #{ret.to_s}" if ret['msg'].to_s.downcase != 'success'
     # return id of the created user
-    ret['data']['ws']['selenium']
+    ret
 end
 
-id = create
+#id = create()
 #delete('jc5fiad')
+
+ret = start('jc5gajl')
+
+url = ret['data']['ws']['selenium']
+opts = Selenium::WebDriver::Chrome::Options.new
+opts.add_option("debuggerAddress", url)
+
+# connect to the existing browser
+# reference: https://localapi-doc-en.adspower.com/docs/K4IsTq
+driver = Selenium::WebDriver.for(:chrome, :options=>opts)
+
+driver.get 'https://google.com'
